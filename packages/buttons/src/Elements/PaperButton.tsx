c7e412cf4a47b4ee9c137a0ed0b10664dd8d07a5
unknown <doniaskima344@gmail.com>
2023-08-10 20:33:20 +0100

    feat(PaperButton :rocket: )

---
+++ packages/buttons/src/Elements/PaperButton.tsx
@@ -0,0 +1,60 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface StyledButtonProps {
+  backgroundColor?: string;
+}
+
+const StyledButton = styled.button<StyledButtonProps>`
+  align-self: center;
+  background-color: ${(props) => props.backgroundColor || '#fff'};
+  background-image: none;
+  background-position: 0 90%;
+  background-repeat: repeat no-repeat;
+  background-size: 4px 3px;
+  border-radius: 15px 225px 255px 15px 15px 255px 225px 15px;
+  border-style: solid;
+  border-width: 2px;
+  box-shadow: rgba(0, 0, 0, 0.2) 15px 28px 25px -18px;
+  box-sizing: border-box;
+  color: #41403e;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Neucha, sans-serif;
+  font-size: 1rem;
+  line-height: 23px;
+  outline: none;
+  padding: 0.75rem;
+  text-decoration: none;
+  transition: all 235ms ease-in-out;
+  border-bottom-left-radius: 15px 255px;
+  border-bottom-right-radius: 225px 15px;
+  border-top-left-radius: 255px 15px;
+  border-top-right-radius: 15px 225px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px -5px;
+    transform: translate3d(0, 2px, 0);
+  }
+
+  &:focus {
+    box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 4px -6px;
+  }
+`;
+
+interface PaperButtonProps {
+  children: ReactNode;
+  backgroundColor?: string;
+}
+
+const PaperButton: React.FC<PaperButtonProps> = ({
+  children,
+  backgroundColor,
+}) => {
+  return <StyledButton backgroundColor={backgroundColor}>{children}</StyledButton>;
+};
+
+export default PaperButton;
