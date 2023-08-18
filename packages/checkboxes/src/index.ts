59fd04a02967ad9a51823ad26aeed14b10b7081d
unknown <doniaskima344@gmail.com>
2023-08-18 12:00:48 +0100

    feat(delete useless file)

--- packages/checkboxes/src/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as CraftifyCheckbox } from "./Elements/CraftifyCheckbox";
-export {default as PathCheckbox } from "./Elements/PathCheckbox";
-export {default as TodoCheckbox } from "./Elements/TodoCheckbox";
-export {default as StyledCheckboxWithTickMark } from "./Elements/StyledCheckboxWithTickMark";
