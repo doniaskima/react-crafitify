79af2d1f719c79edb8db7cd27f8f5028e9c646cf
unknown <doniaskima344@gmail.com>
2023-09-07 19:37:49 +0100

    feat(Dropdown)

--- packages/components/Dropdown/Dropdown.tsx
+++ packages/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Dropdown.scss";
 
 export interface DropdownProps {
@@ -6,87 +6,74 @@ export interface DropdownProps {
   elementList?: React.ReactNode[];
 }
 
-class Dropdown extends Component<DropdownProps> {
-  state = {
-    showDropdown: false,
-    isClosing: false,
+const Dropdown: React.FC<DropdownProps> = ({ position = "bottom-left", elementList = [<span>empty dropdown...🤔</span>], children }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const openDropdown = () => {
+    setShowDropdown(true);
+    setIsClosing(false);
   };
 
-  openDropdown = () => {
-    this.setState({
-      showDropdown: true,
-      isClosing: false,
-    });
-  };
-
-  closeDropDown = async () => {
-    await this.setState({ isClosing: true });
+  const closeDropDown = async () => {
+    setIsClosing(true);
     setTimeout(() => {
-      this.setState({ showDropdown: false });
+      setShowDropdown(false);
     }, 200);
   };
 
-  render() {
-    const { showDropdown, isClosing } = this.state;
+  const animOut = isClosing ? "dropdownAnimOut" : "";
 
-    const animOut = isClosing ? "dropdownAnimOut" : "";
+  const dropdownContent = elementList && (
+    elementList.map((slide, i) => {
+      return React.cloneElement(slide, {
+        key: i,
+        className: "dropElement",
+      });
+    })
+  );
 
-    const dropdownContent = this.props.elementList && (
-      this.props.elementList.map((slide, i) => {
-        return React.cloneElement(slide, {
-          key: i,
-          className: "dropElement",
-        });
-      })
-    );
+  let positionStyle = {
+    top: "100%",
+    left: "0",
+  };
 
-    let positionStyle = {
+  if (position === "bottom-right") {
+    positionStyle = {
       top: "100%",
+      right: "0",
+    };
+  } else if (position === "top-left") {
+    positionStyle = {
+      bottom: "100%",
       left: "0",
     };
-
-    if (this.props.position === "bottom-right") {
-      positionStyle = {
-        top: "100%",
-        right: "0",
-      };
-    } else if (this.props.position === "top-left") {
-      positionStyle = {
-        bottom: "100%",
-        left: "0",
-      };
-    } else if (this.props.position === "top-right") {
-      positionStyle = {
-        bottom: "100%",
-        right: "0",
-      };
-    }
-
-    return (
-      <div className="dropdownWrapper">
-        {showDropdown && (
-          <div onClick={this.closeDropDown} className="dropdownOverlay"></div>
-        )}
-
-        <span onClick={this.openDropdown}>{this.props.children}</span>
-
-        {showDropdown && (
-          <div
-            style={positionStyle}
-            onClick={this.closeDropDown}
-            className={`dropdown ${animOut}`}
-          >
-            {dropdownContent}
-          </div>
-        )}
-      </div>
-    );
+  } else if (position === "top-right") {
+    positionStyle = {
+      bottom: "100%",
+      right: "0",
+    };
   }
-}
 
-Dropdown.defaultProps = {
-  position: "bottom-left",
-  elementList: [<span>empty dropdown...🤔</span>],
+  return (
+    <div className="dropdownWrapper">
+      {showDropdown && (
+        <div onClick={closeDropDown} className="dropdownOverlay"></div>
+      )}
+
+      <span onClick={openDropdown}>{children}</span>
+
+      {showDropdown && (
+        <div
+          style={positionStyle}
+          onClick={closeDropDown}
+          className={`dropdown ${animOut}`}
+        >
+          {dropdownContent}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dropdown;
--- packages/components/stories/Dropdown.stories.tsx
+++ packages/components/stories/Dropdown.stories.tsx
@@ -3,7 +3,7 @@ import { Story, Meta } from "@storybook/react";
 import Dropdown, { DropdownProps } from "../Dropdown/Dropdown";
 
 export default {
-  title: "Dropdown",
+  title: "Components/Dropdown",
   component: Dropdown,
 } as Meta;
 
@@ -14,7 +14,56 @@ DefaultDropdown.args = {
   children: (
     <>
       <button>Click Me</button>
-     
     </>
   ),
+};
+
+export const WithButton = Template.bind({});
+WithButton.args = {
+  children: (
+    <>
+      <button>Click Me</button>
+    </>
+  ),
+  elementList: [
+    <button onClick={() => console.log("Button clicked!")}>Send a message!</button>,
+  ],
+};
+
+export const WithLink = Template.bind({});
+WithLink.args = {
+  children: (
+    <>
+      <button>Click Me</button>
+    </>
+  ),
+  elementList: [
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href=" https://www.npmjs.com/package/react-craftify-spinners"
+    >
+      Go to  https://www.npmjs.com/package/react-craftify-spinners
+    </a>,
+  ],
+};
+
+export const WithMultipleElements = Template.bind({});
+WithMultipleElements.args = {
+  children: (
+    <>
+      <button>Click Me</button>
+    </>
+  ),
+  elementList: [
+    <button onClick={() => console.log("Button clicked!")}>Send a message!</button>,
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href=" https://www.npmjs.com/package/react-craftify-spinners"
+    >
+      Go to  https://www.npmjs.com/package/react-craftify-spinners
+    </a>,
+    
+  ],
 };
