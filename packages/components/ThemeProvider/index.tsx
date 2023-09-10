5b423b46ff0b7b28032cd30a60b29013e9d9e3e8
unknown <doniaskima344@gmail.com>
2023-09-10 15:55:09 +0100

    feat(ThemeProvider :green_heart:)

--- packages/components/ThemeProvider/index.tsx
+++ packages/components/ThemeProvider/index.tsx
@@ -1,3 +1,33 @@
-import React, { Component } from 'react';
- 
- 
+import React, { useState } from 'react';
+import {
+  defaultColor,
+  defaultTextColor,
+  primaryColor,
+  primaryTextColor,
+  successColor,
+  successTextColor,
+  dangerColor,
+  dangerTextColor,
+} from '../styles/index';
+import ThemeContext from '../context/themeContext';
+
+function ThemeProvider({ theme, children }) {
+  const [state, setState] = useState({
+    defaultColor: theme.defaultColor || defaultColor,
+    defaultTextColor: theme.defaultTextColor || defaultTextColor,
+    primaryColor: theme.primaryColor || primaryColor,
+    primaryTextColor: theme.primaryTextColor || primaryTextColor,
+    successColor: theme.successColor || successColor,
+    successTextColor: theme.successTextColor || successTextColor,
+    dangerColor: theme.dangerColor || dangerColor,
+    dangerTextColor: theme.dangerTextColor || dangerTextColor,
+  });
+
+  return (
+    <ThemeContext.Provider value={state}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export default ThemeProvider;
