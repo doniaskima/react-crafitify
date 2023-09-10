fdbe6372aa0367e9894c6b2212befe551fd7c0a7
unknown <doniaskima344@gmail.com>
2023-09-10 12:09:29 +0100

    feat(Statistics)

--- packages/components/Statistic/index.tsx
+++ packages/components/Statistic/index.tsx
@@ -1,68 +1,69 @@
 import React from 'react';
-import  "./Statistic.scss";
- 
-const Statistic = (props) => {
+import './Statistic.scss';
 
-	const { title, value, prevValue, currency, valueClassName } = props;
-
-	const prcntChange = Math.round(((value - prevValue) / prevValue * 100) * 100) / 100;
-
-	const positiveChange = prcntChange >= 0;
-
-	let iconType = "arrowTop";
-	let iconBgColor = "#23d160";
-	if(!positiveChange){
-		iconType = "arrowDown";
-		iconBgColor = "#ff2b2b";
-	}
-
-	return(
-		<div className="statWrapper">
-			<div>
-				<div className="title">{ title }</div>
-			</div>
-
-			<div className="statBottomWrapper">
-
-				<div className="mainVal">
-					<div className={ `value ${ valueClassName }` }>{ numberWithCommas(value) }</div>
-					<div className='currency'>{ currency }</div>
-				</div>
-
-				<div className='otherVal'>
-					<div
-						className='prcntChangeDiv'>
-						<div
-							style={{ backgroundColor: iconBgColor }}
-							className='arrow'>
-							{/* <Icon
-								type={ iconType }
-								color="#fff"
-								size="30px" /> */}
-						</div>
-						<div className="prcntChange">
-							<span>{ positiveChange && "+" }{ prcntChange }%</span>
-						</div>
-					</div>
-					<div className='prevVal'>Previously: { numberWithCommas(prevValue) }</div>
-				</div>
-
-			</div>
-		</div>
-	)
+interface StatisticProps {
+  title?: string;
+  value?: number | undefined;
+  prevValue?: number | undefined;
+  currency?: string | undefined;
+  valueClassName?: string;
 }
 
-Statistic.defaultProps = {
-	title: "",
-	value: undefined,
-	prevValue: undefined,
-	currency: undefined,
-	valueClassName: "",
-}
+const Statistic: React.FC<StatisticProps> = ({
+  title = '',
+  value,
+  prevValue,
+  currency,
+  valueClassName = '',
+}) => {
+  const prcntChange =
+    Math.round(((value - prevValue) / prevValue) * 100 * 100) / 100;
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+  const positiveChange = prcntChange >= 0;
 
+  let iconType = 'arrowTop';
+  let iconBgColor = '#23d160';
+  if (!positiveChange) {
+    iconType = 'arrowDown';
+    iconBgColor = '#ff2b2b';
+  }
+
+  function numberWithCommas(x: number): string {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  return (
+    <div className="statWrapper">
+      <div>
+        <div className="title">{title}</div>
+      </div>
+
+      <div className="statBottomWrapper">
+        <div className="mainVal">
+          <div className={`value ${valueClassName}`}>
+            {numberWithCommas(value || 0)}
+          </div>
+          <div className="currency">{currency}</div>
+        </div>
+
+        <div className="otherVal">
+          <div className="prcntChangeDiv">
+            <div style={{ backgroundColor: iconBgColor }} className="arrow">
+              {/* <Icon
+                type={iconType}
+                color="#fff"
+                size="30px"
+              /> */}
+            </div>
+            <div className="prcntChange">
+              <span>{positiveChange && '+'}{prcntChange}%</span>
+            </div>
+          </div>
+          <div className="prevVal">Previously: {numberWithCommas(prevValue || 0)}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Statistic;
