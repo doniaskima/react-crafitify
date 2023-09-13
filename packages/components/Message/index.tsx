539b588b1a0cb65469b137af631249cbb8524f02
unknown <doniaskima344@gmail.com>
2023-09-13 22:49:38 +0100

    feat(Fixing issues :bug: )

--- packages/components/Message/index.tsx
+++ packages/components/Message/index.tsx
@@ -5,75 +5,70 @@ import Stop from "../icons-craftify/Icons/iconsSvg/StopRed.svg";
 import Exclamation from "../icons-craftify/Icons/iconsSvg/ExclamationBlue.svg";
 import Question from "../icons-craftify/Icons/iconsSvg/QuestionGray.svg";
 
-const message = (text, type, time=2.5) => {
-	// This could be modified using React Portal!!!!
+
+import styles from './Messages.scss';
+
+const message = (text, type, time = 2.5) => {
+
+  time = time * 1000;
+  const animationDelay = 200;
+  const elem = document.querySelectorAll(`.messageWrapper`);
+  if (elem[0]) {
+    elem.forEach((item) => {
+      item.style.display = "none";
+    })
+  }
 
 
-	// Transfert the time in milliseconds
-	time = time * 1000;
-	// Add an animation delay of 0.2s
-	const animationDelay = 200;
-	// Remove the message from the dom if still there
-	const elem = document.querySelectorAll(`.messageWrapper`);
-	if(elem[0]){
-		// Put the display to none if there is already some messages
-		elem.forEach((item) => {
-			item.style.display = "none";
-		})
-	}
+  const outerNode = document.createElement("div");
+  outerNode.className = "messageWrapper";
+  const node = document.createElement("div");
+  node.className = styles.messageDiv;
 
-	// Create the outer div of the message (used to center the message)
-	const outerNode = document.createElement("div");
-	outerNode.className = "messageWrapper";
-	// Create the div of the message
-	const node = document.createElement("div");
-	node.className = styles.messageDiv;
 
-	// create the messageWrapperDiv
-	const messageWrapperDiv = document.createElement("div");
-	messageWrapperDiv.className = "messageWrapper";
+  const messageWrapperDiv = document.createElement("div");
+  messageWrapperDiv.className = "messageWrapper";
 
-	// Check the message type to show the right icon
-	if(type !== "noIcon"){
-		const domImg = document.createElement("img");
-		if(type === "success"){
-			domImg.src = CheckMark;
-		}
-		else if(type === "error"){
-			domImg.src = Stop;
-		}
-		else if(type === "question"){
-			domImg.src = Question;
-		}
-		else {
-			domImg.src = Exclamation;
-		}
-		domImg.className = "msgIcon";
-		node.appendChild(domImg);
-	}
+  if (type !== "noIcon") {
+    const domImg = document.createElement("img");
+    if (type === "success") {
+      domImg.src = CheckMark;
+    }
+    else if (type === "error") {
+      domImg.src = Stop;
+    }
+    else if (type === "question") {
+      domImg.src = Question;
+    }
+    else {
+      domImg.src = Exclamation;
+    }
+    domImg.className = "msgIcon";
+    node.appendChild(domImg);
+  }
 
-	// Add the text to the message
-	const textnode = document.createTextNode(text);
-	node.appendChild(textnode);
-	outerNode.appendChild(node);
-	messageWrapperDiv.appendChild(outerNode)
-	document.body.appendChild(messageWrapperDiv);
 
-	// Set a timeout to remove the message
-	const removeMess = setTimeout(() => {
-		const elem = document.querySelector(`.messageWrapperDiv`);
-		if(elem){
-			elem.parentNode.removeChild(elem);
-		}
-	}, time);
+  const textnode = document.createTextNode(text);
+  node.appendChild(textnode);
+  outerNode.appendChild(node);
+  messageWrapperDiv.appendChild(outerNode)
+  document.body.appendChild(messageWrapperDiv);
 
-	// Set a timeout to add the fadeOut animation
-	const endAnim = setTimeout(() => {
-		const msgDiv = document.querySelector(`.messageWrapper`);
-		if(msgDiv){
-			msgDiv.className = `messageWrapper msgIsFadingOut`;
-		}
-	}, time - animationDelay);
+
+  const removeMess = setTimeout(() => {
+    const elem = document.querySelector(`.messageWrapperDiv`);
+    if (elem) {
+      elem.parentNode.removeChild(elem);
+    }
+  }, time);
+
+
+  const endAnim = setTimeout(() => {
+    const msgDiv = document.querySelector(`.messageWrapper`);
+    if (msgDiv) {
+      msgDiv.className = `messageWrapper msgIsFadingOut`;
+    }
+  }, time - animationDelay);
 }
 
 export default message;
