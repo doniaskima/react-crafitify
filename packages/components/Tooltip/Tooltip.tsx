13e6ace08bf85aaf3dc20d62982049f38a57ff6b
unknown <doniaskima344@gmail.com>
2023-10-01 19:37:12 +0100

    feat: changes :gear:

--- packages/components/Tooltip/Tooltip.tsx
+++ packages/components/Tooltip/Tooltip.tsx
@@ -1,36 +1,36 @@
-import React, { Component, Fragment } from 'react';
-import  "./Tooltip.scss";
+import React, { useState, useContext } from 'react';
+import './Tooltip.scss';
 
+import { GetContext } from '../GetContext';
 
-import GetContext from "../GetContext";
+const Tooltip = () => {
+  const { primaryColor, primaryTextColor } = useContext(GetContext);
 
-class Tooltip extends Component {
+  const [tooltipStyle, setTooltipStyle] = useState('');
 
-	render(){
+  useEffect(() => {
+    if (props.dark) {
+      setTooltipStyle('tooltiptextDark');
+    }
+  }, [props.dark]);
 
-		const { primaryColor, primaryTextColor } = this.props.context;
+  // Manage the position.
+  let position = '';
+  if (props.position === 'bottom') {
+    position = 'bottomTooltip';
+  }
 
-		const tooltipStyle = this.props.dark ? "tooltiptextDark" : "";
+  return (
+    <div className="tooltipWrapper">
+      {props.children}
+      <div
+        className={`tooltip ${position}`}
+        // style={{ transitionDelay: '.25s' }}
+      >
+        <span className={`tooltiptext ${tooltipStyle}`}>{props.text}</span>
+      </div>
+    </div>
+  );
+};
 
-		// Manage the position.
-		let position = "";
-		if(this.props.position === "bottom"){
-			position = "bottomTooltip";
-		}
-
-
-		return (
-			<div className="tooltipWrapper">
-			  { this.props.children }
-				<div
-					// style={{ transitionDelay: ".25s" }}
-					className={ ` tooltip   ${ position }` }>
-				  <span className={ `  tooltiptext   ${ tooltipStyle }` }>{ this.props.text }</span>
-				</div>
-			</div>
-		)
-	}
-}
-
-
-export default GetContext(Tooltip);
+export default Tooltip;
